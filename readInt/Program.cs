a4bea86dbe4307e01fa3849b0bf156261ff48778
Михаил Денисов <migelius.invesy@gmail.com>
2022-05-31 22:01:41 +0300

    Добавьте файлы проекта.

---
+++ readInt/Program.cs
@@ -0,0 +1,31 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        bool isExit = false;
+        int number;
+        Console.WriteLine("Введите любое число:");
+
+        while (isExit == false)
+        {
+            string userInput = Console.ReadLine();
+            number = ReturnNumber(userInput);
+        }
+    }
+
+    static int ReturnNumber(string userInput)
+    {
+        int returnedNumber;
+        bool success = int.TryParse(userInput, out returnedNumber);
+
+        if (success)
+        {
+            Console.WriteLine(returnedNumber);
+            return returnedNumber;
+        }
+        else
+        {
+            Console.WriteLine("Неверные данные!");
+        }
+    }
+}
